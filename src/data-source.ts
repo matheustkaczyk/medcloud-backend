a8c4abfaf2433eecdeba7a3917d3fdfd2ee2e4ee
matheustkaczyk <matheus111904@hotmail.com>
2022-11-10 10:57:34 -0300

    feat: setting up data-source configurations

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,15 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+const { TYPEORM_HOST, TYPEORM_PORT, TYPEORM_USERNAME, TYPEORM_PASSWORD, TYPEORM_DATABASE } = process.env;
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 5432,
-    username: "root",
-    password: "root",
-    database: "medCloud",
+    host: TYPEORM_HOST,
+    port: Number(TYPEORM_PORT),
+    username: TYPEORM_USERNAME,
+    password: TYPEORM_PASSWORD,
+    database: TYPEORM_DATABASE,
     synchronize: true,
     logging: false,
     entities: [],
