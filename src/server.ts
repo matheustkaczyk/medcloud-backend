718e18a8bfb89173aa35838e12f7dc9146f30555
matheustkaczyk <matheus111904@hotmail.com>
2022-11-11 13:59:40 -0300

    feat: setting up jwt middleware

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { JwtAuth } from './utils/jwt';
+
 import { ManagerController } from './controller/ManagerController';
 import { PatientController } from './controller/PatientController';
 
@@ -9,7 +11,7 @@ app.use(express.json());
 app.post('/signup', new ManagerController().createManager);
 app.post('/signin', new ManagerController().authenticateManager);
 
-app.post('/patient', new PatientController().createPatient);
+app.post('/patient', JwtAuth.verifyTokenMiddleware, new PatientController().createPatient);
 app.put('/patient/:id', new PatientController().updatePatient);
 app.get('/patient', new PatientController().getAllPatients);
 app.get('/patient/:id', new PatientController().getPatientById);
