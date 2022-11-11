6fe1d8aaa6b8b5d646d1835a0dc5de812ba03bf3
matheustkaczyk <matheus111904@hotmail.com>
2022-11-11 12:26:48 -0300

    feat: creating createPatient route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ManagerController } from './controller/ManagerController';
+import { PatientController } from './controller/PatientController';
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.use(express.json());
 
 app.post('/signup', new ManagerController().createManager);
 app.post('/signin', new ManagerController().authenticateManager);
+
+app.post('/patient', new PatientController().createPatient);
 
 export default app;
