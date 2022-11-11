7d4973a60353e38aa63fa177f1cfd292f1ad1f12
matheustkaczyk <matheus111904@hotmail.com>
2022-11-11 12:02:30 -0300

    feat: createPatient method

--- src/service/PatientService.ts
+++ src/service/PatientService.ts
@@ -1,5 +1,29 @@
+import { Patient } from '@prisma/client';
 import { prisma } from '../../prisma/client';
+import { CreatePatientDto } from '../dto/PatientDto';
 
 export class PatientService {
+  public async createPatient({ firstName, lastName, email, password, address }: CreatePatientDto): Promise<Patient> {
+    const patientExists = await prisma.patient.findUnique({
+      where: {
+        email,
+      },
+    });
 
+    if (patientExists) {
+      throw new Error('Patient already exists');
+    }
+
+    const newPatient = await prisma.patient.create({
+      data: {
+        name: firstName,
+        last_name: lastName,
+        email,
+        password,
+        address,
+      },
+    });
+
+    return newPatient;
+  }
 }
