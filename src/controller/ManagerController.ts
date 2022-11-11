a5869ecaf002a74dac66e6ac559af45acb7ecfdb
matheustkaczyk <matheus111904@hotmail.com>
2022-11-11 09:59:54 -0300

    feat: adding authenticate method on controller

--- src/controller/ManagerController.ts
+++ src/controller/ManagerController.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from "express";
-import { ManagerDto } from "../dto/ManagerDto";
+import { AuthenticateManagerDto, CreateManagerDto } from "../dto/ManagerDto";
 import { ManagerService } from "../service/ManagerService";
 
 export class ManagerController {
   public async createManager(req: Request, res: Response) {
-    const { firstName, lastName, email, password }: ManagerDto = req.body;
+    const { firstName, lastName, email, password }: CreateManagerDto = req.body;
 
     try {
       const managerService = new ManagerService();
       const manager = await managerService.createManager({ firstName, lastName, email, password });
       res.status(201).json(manager);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  public async authenticateManager(req: Request, res: Response) {
+    const { email, password }: AuthenticateManagerDto = req.body;
+
+    try {
+      const managerService = new ManagerService();
+      const manager = await managerService.authenticateManager({ email, password });
+      res.status(200).json(manager);
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
