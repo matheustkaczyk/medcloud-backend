89a1a6794a3bed95c1258fa38388ebe31b3a4b69
matheustkaczyk <matheus111904@hotmail.com>
2022-11-12 08:54:25 -0300

    feat: calling up patient validation methods

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { ManagerController } from './controller/ManagerController';
 import { PatientController } from './controller/PatientController';
 
 import { ManagerValidation } from './middleware/ManagerValidation';
+import { PatientValidation } from './middleware/PatientValidation';
 
 const app = express();
 
@@ -14,8 +15,8 @@ app.use(express.json());
 app.post('/signup', ManagerValidation.createManagerMiddleware, new ManagerController().createManager);
 app.post('/signin', ManagerValidation.authenticateManagerMiddleware, new ManagerController().authenticateManager);
 
-app.post('/patient', JwtAuth.verifyTokenMiddleware, new PatientController().createPatient);
-app.put('/patient/:id', JwtAuth.verifyTokenMiddleware, new PatientController().updatePatient);
+app.post('/patient', PatientValidation.createOrEditPatientMiddleware, JwtAuth.verifyTokenMiddleware, new PatientController().createPatient);
+app.put('/patient/:id', PatientValidation.createOrEditPatientMiddleware, JwtAuth.verifyTokenMiddleware, new PatientController().updatePatient);
 app.get('/patient', JwtAuth.verifyTokenMiddleware, new PatientController().getAllPatients);
 app.get('/patient/:id', JwtAuth.verifyTokenMiddleware, new PatientController().getPatientById);
 app.delete('/patient/:id', JwtAuth.verifyTokenMiddleware, new PatientController().deletePatientById);
