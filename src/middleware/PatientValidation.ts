62f38b67e8458a68d7d4dd6584f81fd731df61cc
matheustkaczyk <matheus111904@hotmail.com>
2022-11-12 09:02:07 -0300

    refact: splitting into two validations of patient

--- src/middleware/PatientValidation.ts
+++ src/middleware/PatientValidation.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response } from "express";
-import { CreatePatientSchema } from "../utils/validationSchemas/patientSchemas";
+import { CreatePatientSchema, UpdatePatientSchema } from "../utils/validationSchemas/patientSchemas";
 
 export class PatientValidation {
-  public static createOrEditPatientMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  public static createPatientMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const { firstName, lastName, email, password, address } = req.body;
 
     const { error } = CreatePatientSchema.validate({ firstName, lastName, email, password, address });
+
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    next();
+  };
+
+  public static updatePatientMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const { firstName, lastName, password, address } = req.body;
+
+    const { error } = UpdatePatientSchema.validate({ firstName, lastName, password, address });
 
     if (error) {
       return res.status(400).json({ error: error.message });
